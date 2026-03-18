03412095cd7167ca7aebc9e896d7399fec81ff9b
infobeehunter <info.beehunter@gmail.com>
2026-03-18 14:16:28 +0100

    Delete AppColors.js

--- AppColors.js
+++
@@ -1,9 +0,0 @@
-// AppColors.js
-export const COLORS = {
- primary: '#0047AB', // Blu del pin (Adriatic Blue)
- secondary: '#FFD700', // Giallo dell'ape (Golden Yellow)
- dark: '#000000', // Nero del testo e dettagli
- light: '#FFFFFF', // Bianco di sfondo
- gray: '#F5F5F5', // Grigio chiaro per i menu
- accent: '#FF4500' // Arancione/Rosso per i badge speciali
-};
